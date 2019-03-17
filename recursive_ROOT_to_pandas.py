c3fd8ca58641673ef14c7e500c602906a83f3a9a
nhaubrich <nicholasjhaubrich@gmail.com>
2019-03-17 17:05:30 -0400

    Added progress bar

--- recursive_ROOT_to_pandas.py
+++ recursive_ROOT_to_pandas.py
@@ -1,11 +1,10 @@
 #recursively combine root files to pandas by sample
-
 import glob
 from root_pandas import read_root
 import pandas as pd
 import sys
 import os
-import time
+from tqdm import tqdm
 
 #NOTE: Accessing an element of a vector seems to return a list with one element (e.g. Jet_pt[0] will be [250.0], not 250), so each column of this sort needs to be converted via .str[0]
 cols = ['V_pt']#,'noexpand:Jet_pt[hJetInd1]']#,'Jet_pt']#,'FatJet_pt']
@@ -19,16 +18,18 @@ for n,sample in enumerate(glob.glob(sys.argv[1]+"*/")):
         break
     head, tail = os.path.split(sample)
     samplename = head.split(os.sep)[-1]
+    print(samplename)
+    
     dfs = []
-    for m,filename in enumerate(glob.glob(sample+"*.root")):
+    for m,filename in tqdm(enumerate(glob.glob(sample+"*.root"))):
         count+=1
         if count>100:
+            print("stopping...")
             stop=True
             break
         head, tail = os.path.split(filename)
-        print(tail)
+        #print(tail)
         dfs.append(read_root(filename,"Events", columns=cols))
 
     df = pd.concat(dfs)
     df.to_hdf("pandas_files/"+samplename+".h5",key="Events")
-
