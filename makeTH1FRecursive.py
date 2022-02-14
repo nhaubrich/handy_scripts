8246348fb3e224ca5ce90b934b3e594f721daf9a
Nick Haubrich <nicholasjhaubrich@gmail.com>
2022-02-14 14:45:34 -0600

    Add recursive TH1D-to-TH1F conversion

---
+++ makeTH1FRecursive.py
@@ -0,0 +1,31 @@
+import ROOT
+import os
+import sys
+
+fname = sys.argv[1]
+f = ROOT.TFile(fname,"UPDATE")
+
+def convertToTH1F(f,key):
+    obj = getattr(f,key)
+    print(key)
+    if isinstance(obj,ROOT.TDirectoryFile):
+        #print("TD")
+        #directory, so call recursively on each subsequent object
+        for subkey in obj.GetListOfKeys():
+            convertToTH1F(obj,subkey.GetName())
+    
+    if isinstance(obj,ROOT.TH1D):
+        #print("TH1D")
+        h1F = ROOT.TH1F()
+        obj.Copy(h1F)
+        f.cd()
+        h1F.Write()
+        f.Delete(obj.GetName()+";1")
+
+    
+
+for key in f.GetListOfKeys():
+    print(key)
+    convertToTH1F(f,key.GetName())
+
+f.Close()
